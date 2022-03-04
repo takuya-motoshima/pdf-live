e5ebb573425b8f1b6ae743134830ff2e4d65594b
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2022-03-04 17:13:51 +0900

    Added layout display processing for zoom overlay.

--- src/script.js
+++ src/script.js
@@ -1,16 +1,6 @@
 import loadingModal from './loadingModal.js';
 
 (async () => {
-
-  // Show loading.
-  loadingModal.open();
-
-  const leftPanel = document.querySelector('[data-element="leftPanel"]');
-  const leftPanelButton = document.querySelector('[data-element="leftPanelButton"]');
-  const documentContentContainer = document.querySelector('[data-element="documentContentContainer"]');
-  const leftPanelCloseButton = document.querySelector('[data-element="leftPanelCloseButton"]');
-  const zoomOverlay = document.querySelector('[data-element="zoomOverlay"]');
-
   /**
   * Open the left panel.
   */
@@ -29,14 +19,6 @@ import loadingModal from './loadingModal.js';
     documentContentContainer.classList.remove('open')
   }
 
-  // Toggle the opening and closing of the left panel.
-  leftPanelButton.addEventListener('click', () => {
-    if (leftPanel.classList.contains('closed'))
-      openLeftPanel();
-    else
-      closeLeftPanel();
-  });
-
   /**
     * Init PDF viewer.
     */
@@ -54,39 +36,118 @@ import loadingModal from './loadingModal.js';
   async function renderAllPages(pdf) {
     console.log(`pdf.numPages=${pdf.numPages}`);
     for (let pageNum=1; pageNum<=pdf.numPages; pageNum++) {
-      await renderPage(pageNum);
+      await renderPage(pdf, pageNum);
       console.log(`Render the ${pageNum}th page`);
     }
   }
 
   /**
+   * Layout the zoom overlay.
+   */
+  function layoutZoomOverlay() {
+    const rect =  toggleZoomOverlay.getBoundingClientRect();
+    zoomOverlay.style.top = `${rect.bottom}px`;
+    zoomOverlay.style.left = `${rect.left}px`;
+  }
+
+  /**
+   * Check if there is a point inside the rectangle.
+   */
+  function pointInRectangle(point, rect) {
+    // console.log(`point=${JSON.stringify(point, null, 2)}`);
+    return rect.x <= point.x && point.x <= rect.x + rect.width && rect.y <= point.y && point.y <= rect.y + rect.height;
+  }
+
+  /**
     * Render the specified page of PDF.
     */
-  async function renderPage(pageNum) {
+  async function renderPage(pdf, pageNum) {
     // Fetch the specified page.
     const page = await pdf.getPage(pageNum);
 
     // Support HiDPI-screens.
     const outputScale = window.devicePixelRatio || 1;
     const viewport = page.getViewport({scale: 1.5});
-    const canvas = $('<canvas/>').appendTo(viewer)
-      .attr({width: Math.floor(viewport.width * outputScale), height: Math.floor(viewport.height * outputScale)})
-      .css({width: Math.floor(viewport.width), height: Math.floor(viewport.height)});
+
+    // Create PDF page elements.
+    const section = document.createElement('div');
+    section.id = `section${pageNum}`;
+    section.classList.add('pageSection');
+
+    const container = document.createElement('div');
+    container.id = `pageContainer${pageNum}`;
+    container.classList.add('pageContainer');
+
+    const canvas = document.createElement('canvas');
+    canvas.width = Math.floor(viewport.width * outputScale);
+    canvas.height = Math.floor(viewport.height * outputScale);
+    canvas.style.width = `${Math.floor(viewport.width)}px`;
+    canvas.style.height = `${Math.floor(viewport.height)}px`;
+
+    container.appendChild(canvas);
+    section.appendChild(container);
+    viewer.appendChild(section);
+
+    // Draw contents of a PDF file to a Canvas.
     page.render({
-      canvasContext: canvas.get(0).getContext('2d'),
+      canvasContext: canvas.getContext('2d'),
       transform: outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null,
-      viewport: viewport
+      viewport
     });
   }
+
+  // Show loading.
+  loadingModal.open();
+
+  const leftPanel = document.querySelector('[data-element="leftPanel"]');
+  const leftPanelButton = document.querySelector('[data-element="leftPanelButton"]');
+  const documentContentContainer = document.querySelector('[data-element="documentContentContainer"]');
+  const leftPanelCloseButton = document.querySelector('[data-element="leftPanelCloseButton"]');
+  const toggleZoomOverlay = document.querySelector('[data-element="toggleZoomOverlay"]');
+  const zoomOverlay = document.querySelector('[data-element="zoomOverlay"]');
+  const viewer = document.querySelector('[data-element="viewer"]');
+
+  // Toggle the opening and closing of the left panel.
+  leftPanelButton.addEventListener('click', () => {
+    if (leftPanel.classList.contains('closed'))
+      openLeftPanel();
+    else
+      closeLeftPanel();
+  });
 
   // Close left panel.
   leftPanelCloseButton.addEventListener('click', () => {
     closeLeftPanel();
   });
 
-  // Open the zoom panel.
-  zoomOverlay.addEventListener('click', () => {
-    closeLeftPanel();
+  // Toggle the opening and closing of the zoom overlay.
+  toggleZoomOverlay.addEventListener('click', (evnt) => {
+    if (zoomOverlay.classList.contains('closed')) {
+      // Stops event propagation to the body so that the process of closing the zoom overlay for body click events does not run.
+      evnt.stopPropagation();
+
+      // Open zoom overlay.
+      // Layout the zoom overlay.
+      layoutZoomOverlay();
+
+      // Open zoom overlay.
+      zoomOverlay.classList.remove('closed');
+    } else
+      // Close zoom overlay.
+      zoomOverlay.classList.add('closed');
+  });
+
+  // Recalculate the layout and size of each element when the window is resized.
+  window.addEventListener('resize', () => {
+    // Layout the zoom overlay.
+    layoutZoomOverlay();
+  });
+
+  // When the screen is clicked.
+  document.body.addEventListener('click', evnt => {
+    // Close if zoom overlay is open.
+    if (!zoomOverlay.classList.contains('closed') && !pointInRectangle({x: evnt.pageX, y: evnt.pageY}, zoomOverlay.getBoundingClientRect()))
+      zoomOverlay.classList.add('closed');
   });
 
   // Init PDF viewer.
