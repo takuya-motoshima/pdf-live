345a20a6c4adb1a7c5807e877d040ff47d8ffc97
Takuya Motoshima <developer.takuyamotoshima@gmail.com>
2022-03-07 08:00:11 +0900

    Clean up CSS.

--- src/loadingModal.js
+++ src/loadingModal.js
@@ -2,7 +2,7 @@ import Modal from './Modal.js';
 
 export default new (class extends Modal {
   render() {
-    return `<div class="Modal loading-modal closed">
+    return `<div class="modal loading-modal closed">
               <div class="container">
                 <div class="inner-wrapper"></div>
               </div>
